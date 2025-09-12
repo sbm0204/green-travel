41f15561e106008b7ce3956753118ad69f40f4de
sbm0204 <song.bm24@gmail.com>
2025-09-12 11:13:25 +0900

    250912 router 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css';
 import Header from './components/common/Header';
+import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <main>
+        <Outlet></Outlet>
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
